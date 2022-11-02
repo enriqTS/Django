ed093b0e33a3a7d24e29c769b9656b55b39deb02
Henrique <henrcraft@gmail.com>
2022-11-02 14:44:44 -0300

    Simple word counter

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -4,4 +4,14 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return render(request, 'index.html')
+    context = {
+        'name': 'Patrick',
+        'age': 23,
+        'nationalitty': 'british'
+    }
+    return render(request, 'index.html', context)
+
+def counter(request):
+    text = request.GET['text']
+    amount_of_words = len(text.split())
+    return render(request, 'counter.html', {'amount': amount_of_words})    
